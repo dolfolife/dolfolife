94499746e360d151b821597f36b6b777de6b7743
Rodolfo Sanchez <rodolfo2488@gmail.com>
2022-03-12 16:12:59 -0600

    doc: mark TODO for things I need to improve
    Signed-off-by: Rodolfo Sanchez <rodolfo2488@gmail.com>

--- resume/build.sh
+++ resume/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+
 docker build --no-cache -t dolfo/latex:$(cat ./VERSION) .
 
+# TODO why  do  ineed to run pdflatex twice
 # Double to procress refs
 docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/resume dolfo/latex:$(cat ./VERSION) /bin/sh -c "pdflatex resume.tex && pdflatex resume.tex"
