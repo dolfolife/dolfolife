8370f78a375c51d0c0286d268db4f1b6c04b0a80
Rodolfo Sanchez <rodolfo2488@gmail.com>
2022-03-14 01:28:08 -0500

    moved to nextjs
    feat: let's use nextjs to learn :) no other specific reason

---
+++ dolfo.codes/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Image from 'next/image'
+
+
+const Home: NextPage = () => {
+  return (
+        <h1 >
+          Dolfo Codes
+        </h1>
+  )
+}
+
+export default Home
