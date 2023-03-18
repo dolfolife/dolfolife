c6837ecdb7d6937752789e995759ffd92b3e9aaa
Rodolfo Sanchez <rodolfo2488@gmail.com>
2023-03-18 11:31:21 -0500

    added local test script
    Signed-off-by: Rodolfo Sanchez <rodolfo2488@gmail.com>

--- resume/build.sh
+++ resume/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker build --no-cache -t dolfo/latex:$(cat ./VERSION) .
+docker build --no-cache -t dolfolife/latex:$(cat ./VERSION) .
 
 # TODO why  do  ineed to run pdflatex twice
 # Double to procress refs
-docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/resume dolfo/latex:$(cat ./VERSION) /bin/sh -c "pdflatex resume.tex && pdflatex resume.tex"
+docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/resume dolfolife/latex:$(cat ./VERSION) /bin/sh -c "pdflatex resume.tex && pdflatex resume.tex"
